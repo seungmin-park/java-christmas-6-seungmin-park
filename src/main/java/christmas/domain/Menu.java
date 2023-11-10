cd8b83974e13ff3777d993c8b4c8c8cc29629dec
박승민 <tmddudals369@naver.com>
2023-11-11 04:33:57 +0900

    feat(menu): 메뉴를 관리한 enum 구현

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,30 @@
+package christmas.domain;
+
+import java.util.Arrays;
+
+public enum Menu {
+    BUTTON_MUSHROOM_SOUP("앙송이수프", 6000),
+    TAPAS("타파스", 5_500),
+    CAESAR_SALAD("시저샐러드", 8_000),
+    T_BORN_STEAK("티본스테이크", 55_000),
+    BARBECUE_RIBS("바비큐립", 54_000),
+    SEAFOOD_PASTA("해산물파스타", 35_000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000),
+    CHOCOLATE_CAKE("초코케이크", 15_000),
+    ICE_CREAM("아이스크림", 5_000),
+    ZERO_COLA("제로콜라", 3_000),
+    RED_WINE("레드와인", 60_000),
+    CHAMPAGNE("샴페인", 25_000);
+    private final String name;
+    private final int price;
+
+    Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Menu findByMenuName(String menuName) {
+        return Arrays.stream(Menu.values()).filter(menu -> menu.name.equals(menuName)).findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."));
+    }
+}
