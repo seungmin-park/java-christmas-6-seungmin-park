afa2fb475ed39e321aafa3398e39da6e22086201
박승민 <tmddudals369@naver.com>
2023-11-12 19:16:18 +0900

    feat(menu): 메뉴의 금액을 가져오기 위한 getter 추가

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -29,4 +29,8 @@ public enum Menu {
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."));
     }
+
+    public int getPrice() {
+        return price;
+    }
 }
