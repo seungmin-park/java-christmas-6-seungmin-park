b3654ceea68bb3c64803afb3ffb9c2ca718f5d0d
박승민 <tmddudals369@naver.com>
2023-11-14 04:50:27 +0900

    test(money): 돈의 금액이 일정 금액보다 크거나 같은지 비교하는 기능 테스트

--- src/test/java/christmas/domain/MoneyTest.java
+++ src/test/java/christmas/domain/MoneyTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MoneyTest {
 
@@ -31,4 +33,29 @@ class MoneyTest {
         assertThatThrownBy(() -> new Money(amount))
             .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @ParameterizedTest
+    @CsvSource({"1000, 1000", "1000,999"})
+    @DisplayName("돈의 금액이 비교 금액보다 크거나 같으면 true를 반환한다.")
+    void returnTrueAmountGreaterThanEqualMoneyAmount(int moneyAmount, int targetAmount) {
+        //given
+        Money money = new Money(moneyAmount);
+        //when
+        boolean result = money.isGreaterThanEqualsAmount(targetAmount);
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("돈의 금액이 비교 금액보다 작으면 false를 반환한다.")
+    void returnFalseAmountLessMoneyAmount() {
+        //given
+        Money money = new Money(1000);
+        int amount = 1001;
+        //when
+        boolean result = money.isGreaterThanEqualsAmount(amount);
+        //then
+        assertThat(result).isFalse();
+    }
+
 }
