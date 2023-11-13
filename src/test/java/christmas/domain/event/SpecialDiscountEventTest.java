6b1d81da5b02c053b11d361a332b1c560c2542b7
박승민 <tmddudals369@naver.com>
2023-11-14 04:03:53 +0900

    test(specialDiscountEvent): 특별 할인 이벤트 조건에 만족하는지 판단하는 기능 테스트

---
+++ src/test/java/christmas/domain/event/SpecialDiscountEventTest.java
@@ -0,0 +1,43 @@
+package christmas.domain.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.Menu;
+import christmas.domain.Money;
+import christmas.domain.Order;
+import java.time.LocalDate;
+import java.util.EnumMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SpecialDiscountEventTest {
+
+    @Test
+    @DisplayName("이벤트 조건에 만족할 경우 true를 반환한다.")
+    void returnTrueOrderSatisfiedByEventCondition() {
+        //given
+        LocalDate orderDate = LocalDate.of(2023, 12, 3);
+        Map<Menu, Integer> menus = new EnumMap<>(Menu.class);
+        Order order = new Order(orderDate, menus);
+        SpecialDiscountEvent specialDiscountEvent = new SpecialDiscountEvent();
+        //when
+        boolean result = specialDiscountEvent.isSatisfiedBy(order);
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("이벤트 조건에 만족하지 않을 경우 false를 반환한다.")
+    void returnFalseOrderNotSatisfiedByEventCondition() {
+        //given
+        LocalDate orderDate = LocalDate.of(2023, 12, 1);
+        Map<Menu, Integer> menus = new EnumMap<>(Menu.class);
+        Order order = new Order(orderDate, menus);
+        SpecialDiscountEvent specialDiscountEvent = new SpecialDiscountEvent();
+        //when
+        boolean result = specialDiscountEvent.isSatisfiedBy(order);
+        //then
+        assertThat(result).isFalse();
+    }
+}
