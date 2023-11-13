77bebe9ed0cc64c31eaaf671fa8232fcbacb1eda
박승민 <tmddudals369@naver.com>
2023-11-14 03:20:35 +0900

    test(dayDiscountEvent): 이벤트 당일까지 남은 기간에 대해 할인된 금액을 반환하는 기능 테스트

--- src/test/java/christmas/domain/event/DayDiscountEventTest.java
+++ src/test/java/christmas/domain/event/DayDiscountEventTest.java
@@ -3,12 +3,15 @@ package christmas.domain.event;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import christmas.domain.Menu;
+import christmas.domain.Money;
 import christmas.domain.Order;
 import java.time.LocalDate;
 import java.util.EnumMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class DayDiscountEventTest {
 
@@ -38,5 +41,20 @@ class DayDiscountEventTest {
         boolean result = dayDiscountEvent.isSatisfiedBy(order);
         //then
         assertThat(result).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2023-12-01, 1000", "2023-12-25, 3400"})
+    @DisplayName("할인금액을 반환한다.")
+    void returnDiscountedMoney(LocalDate orderDate, int discountedAmount) {
+        //given
+        Map<Menu, Integer> menus = new EnumMap<>(Menu.class);
+        Order order = new Order(orderDate, menus);
+        DayDiscountEvent dayDiscountEvent = new DayDiscountEvent();
+        //when
+        Money discountedMoney = dayDiscountEvent.apply(order);
+        //then
+        assertThat(discountedMoney).extracting("amount")
+            .isEqualTo(discountedAmount);
     }
 }
