825b201edd12913380ab211bc0bf4abe3ecd0ed8
박승민 <tmddudals369@naver.com>
2023-11-14 03:08:55 +0900

    feat(dayDiscountEvent): 이벤트 당일까지 남은 기간동안 할인하는 이벤트 구현

---
+++ src/main/java/christmas/domain/event/DayDiscountEvent.java
@@ -0,0 +1,25 @@
+package christmas.domain.event;
+
+import christmas.domain.Money;
+import christmas.domain.Order;
+import java.time.LocalDate;
+
+public class DayDiscountEvent implements Event{
+    private static final LocalDate startDate = LocalDate.of(2023, 12, 1);
+    private static final LocalDate endDate = LocalDate.of(2023, 12, 25);
+    private static final int D_DAY_LEAST_DISCOUNT_AMOUNT = 1_000;
+    private static final int D_DAY_DISCOUNT_POINT = 100;
+    @Override
+    public boolean isSatisfiedBy(Order order) {
+        return order.isBetweenAt(startDate, endDate);
+    }
+
+    @Override
+    public Money apply(Order order) {
+        return new Money(calculateDiscountAmount(order));
+    }
+
+    private int calculateDiscountAmount(Order order) {
+        return D_DAY_LEAST_DISCOUNT_AMOUNT + (order.getOrderDayOfMonth() - 1) * D_DAY_DISCOUNT_POINT;
+    }
+}
