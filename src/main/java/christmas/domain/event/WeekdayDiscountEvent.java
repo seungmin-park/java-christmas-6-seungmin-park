33b21da09f73b3ddbac47cccb2745ee9000194ba
박승민 <tmddudals369@naver.com>
2023-11-15 02:08:11 +0900

    feat(weekdayDiscountEvent): 평일에 디저트 메뉴를 주문할 경우 할인해주는 이벤트 구현

---
+++ src/main/java/christmas/domain/event/WeekdayDiscountEvent.java
@@ -0,0 +1,33 @@
+package christmas.domain.event;
+
+import christmas.domain.MenuType;
+import christmas.domain.Money;
+import christmas.domain.Order;
+import java.time.DayOfWeek;
+import java.util.List;
+
+public class WeekdayDiscountEvent implements Event {
+    private static final List<DayOfWeek> WEEK_DAY = List.of(
+        DayOfWeek.SUNDAY,
+        DayOfWeek.MONDAY,
+        DayOfWeek.TUESDAY,
+        DayOfWeek.WEDNESDAY,
+        DayOfWeek.THURSDAY
+    );
+    private static final MenuType EVENT_MENU_TYPE = MenuType.DESSERT;
+    private static final int DISCOUNT_AMOUNT = 2_023;
+
+    @Override
+    public boolean isSatisfiedBy(Order order) {
+        return order.isMatchedDayOfWeek(WEEK_DAY) && order.containMenuType(MenuType.DESSERT);
+    }
+
+    @Override
+    public Money apply(Order order) {
+        return new Money(calculateDiscountAmount(order));
+    }
+
+    private int calculateDiscountAmount(Order order) {
+        return order.getMenuTypeCount(EVENT_MENU_TYPE) * DISCOUNT_AMOUNT;
+    }
+}
