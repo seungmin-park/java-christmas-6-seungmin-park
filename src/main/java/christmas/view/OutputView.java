fb55bbf0d9b13010dd7dfe3ae7e1973f3513202e
박승민 <tmddudals369@naver.com>
2023-11-15 03:52:25 +0900

    feat(outputView): 이벤트 플래너 안내 메세지를 출력하는 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+public class OutputView {
+
+    private static final String PLANNER_NOTICE_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public void printPlannerNoticeMessage() {
+        System.out.println(PLANNER_NOTICE_MESSAGE);
+    }
+}
