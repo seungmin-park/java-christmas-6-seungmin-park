0e89c2ed82160d7eab18b28400d4d377aebaa607
박승민 <tmddudals369@naver.com>
2023-11-15 15:30:44 +0900

    feat(inputView): 주문할 메뉴와 개수를 입력받는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,10 +2,12 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.utils.DateValidator;
+import christmas.utils.OrderValidator;
 
 public class InputView {
 
     private static final String VISIT_DATE_INPUT_GUIDE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String ORDER_MENU_INPUT_GUIDE_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public String inputVisitDate() {
         System.out.println(VISIT_DATE_INPUT_GUIDE_MESSAGE);
@@ -13,5 +15,12 @@ public class InputView {
         DateValidator.validateVisitDate(visitDate);
         DateValidator.validateInRangeDate(visitDate);
         return visitDate;
+    }
+
+    public String inputOrders() {
+        System.out.println(ORDER_MENU_INPUT_GUIDE_MESSAGE);
+        String orders = Console.readLine();
+        OrderValidator.validateOrders(orders);
+        return orders;
     }
 }
