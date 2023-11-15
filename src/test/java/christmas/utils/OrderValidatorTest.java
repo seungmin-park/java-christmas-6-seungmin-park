c35e2f884533a7c3e6a75cff31f518882ff06242
박승민 <tmddudals369@naver.com>
2023-11-15 15:07:19 +0900

    feat(orderValidator): 주문의 형태가 유효한 형태인지 검증하는 기능 테스트

--- src/test/java/christmas/utils/OrderValidatorTest.java
+++ src/test/java/christmas/utils/OrderValidatorTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class OrderValidatorTest {
 
@@ -37,6 +39,27 @@ class OrderValidatorTest {
         String order = "타파스-1";
         //when //then
         assertThatCode(() -> OrderValidator.validateOrders(order))
+            .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"''", "' 타파스-1'", "타파스-0"})
+    @DisplayName("주문 형태가 올바르지 않은 경우 예외를 발생시킨다.")
+    void throwExceptionInCorrectOrderFormat(String order) {
+        //given
+        //when //then
+        assertThatThrownBy(() -> OrderValidator.validateOrder(order))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("주문 형태가 올바른 경우 예외를 발생시키지 않는다.")
+    void notThrowExceptionCorrectOrderFormat() {
+        //given
+        String order = "타파스-1";
+        //when //then
+        assertThatCode(() -> OrderValidator.validateOrder(order))
             .doesNotThrowAnyException();
     }
 }
