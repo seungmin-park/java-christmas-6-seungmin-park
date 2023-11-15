2688402d37034ec5d64fa6817de4c7d1f0449595
박승민 <tmddudals369@naver.com>
2023-11-15 17:30:15 +0900

    feat(bill): 증정 메뉴를 반환하는 기능 구현

--- src/main/java/christmas/domain/Bill.java
+++ src/main/java/christmas/domain/Bill.java
@@ -17,8 +17,23 @@ public class Bill {
         return new Money(order.getTotalPrice());
     }
 
+    public Menu getGiftMenu() {
+        if (isContainGifEvent()) {
+            GiftEvent giftEvent = getGiftEvent();
+            return giftEvent.getGiftMenu();
+        }
+        return Menu.NONE;
+    }
+
     public boolean isContainGifEvent() {
         return events.stream()
             .anyMatch(event -> event instanceof GiftEvent);
+    }
+
+    private GiftEvent getGiftEvent() {
+        return (GiftEvent) events.stream()
+            .filter(event -> event instanceof GiftEvent)
+            .findFirst()
+            .get();
     }
 }
