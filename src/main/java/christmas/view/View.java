a1491944f0ebc59ba8d407556980d0f8fa733241
박승민 <tmddudals369@naver.com>
2023-11-15 15:54:33 +0900

    feat(view): 주문할 메뉴와 개수가 유효한 값이 아닌 경우 입력을 다시 받는 기능 구현

--- src/main/java/christmas/view/View.java
+++ src/main/java/christmas/view/View.java
@@ -1,5 +1,13 @@
 package christmas.view;
 
+import christmas.domain.Menu;
+import christmas.utils.OrderValidator;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class View {
     private final InputView inputView;
     private final OutputView outputView;
@@ -19,5 +27,36 @@ public class View {
                 outputView.printErrorMessage(e.getMessage());
             }
         }
+    }
+
+    public Map<Menu, Integer> getMenuAndCount() {
+        while (true) {
+            try {
+                String orders = inputView.inputOrders();
+                List<String> convertedOrders = convertToList(orders);
+                convertedOrders.forEach(OrderValidator::validateOrder);
+                return convertToMenuMap(convertedOrders);
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private List<String> convertToList(String orders) {
+        return Arrays.stream(orders.split(",", -1))
+            .toList();
+    }
+
+    private EnumMap<Menu, Integer> convertToMenuMap(List<String> convertedOrders) {
+        return convertedOrders.stream()
+            .map(order -> order.split("-"))
+            .collect(Collectors.toMap(
+                menuAndCount -> Menu.findByMenuName(menuAndCount[0]),
+                menuAndCount -> Integer.parseInt(menuAndCount[1]),
+                (existing, replacement) -> {
+                    throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                },
+                () -> new EnumMap<>(Menu.class)
+            ));
     }
 }
