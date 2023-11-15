49d0641d6e341c3ce4927e54a63ef0a7cda3800d
박승민 <tmddudals369@naver.com>
2023-11-15 16:43:32 +0900

    feat(outputView): 주문 메뉴와 개수를 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,8 @@
 package christmas.view;
 
+import christmas.domain.Menu;
+import java.util.Map;
+
 public class OutputView {
 
     private static final String PLANNER_NOTICE_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
@@ -14,5 +17,11 @@ public class OutputView {
 
     public void printBenefitContextNoticeMessage() {
         System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+
+    public void printOrderMenuAndCount(Map<Menu, Integer> orderMenus) {
+        System.out.println("\n<주문 메뉴>");
+        orderMenus.keySet()
+            .forEach(menu -> System.out.printf("%s %d개\n", menu.getName(), orderMenus.get(menu)));
     }
 }
